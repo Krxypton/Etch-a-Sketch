dbbc5fca81e4859f9c51aed4587dd3f0ea16edf7
Krxypton <krxypton@gmail.com>
2025-08-29 21:38:55 +0100

    Done

--- script.js
+++ script.js
@@ -1,14 +1,45 @@
 createGrid();
 
-function createGrid(){
+
+function createGrid() {
+    let size = gridSize();
     const X = document.querySelector(".X");
 
-    for(let x = 0; x <16; x++){
-        let row = "";
-        for(let i = x; i < 16; i++) {
-        row += document.createElement("div");
-        
-        }
-        X.appendChild(row);
+    while(X.firstChild) {
+        X.removeChild(X.firstChild);
     }
+
+    for(let x = 0; x <size; x++){
+        const row = document.createElement("div");
+        X.appendChild(row);
+        for(let i = 0; i < size; i++) {
+        const gridsX = document.createElement("div");
+        row.appendChild(gridsX);
+        }
+    }
+}
+
+ let grid = document.querySelectorAll(".X");
+
+grid.forEach(grids => {
+grids.addEventListener("mousemove", (e) => {
+    hover(e);
+})
+})
+
+function hover(e) {
+    e.target.style.backgroundColor = "purple";
+    return;
+}
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", (e) => {
+    createGrid();
+
+})
+
+function gridSize() {
+    let answer = prompt("What do you want the grid size to be?");
+    return answer;
 }
